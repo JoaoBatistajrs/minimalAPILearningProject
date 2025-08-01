854849c1e006f6844880c108a1138756e85f6619
Joao Batista <joao.batista.jrs@gmail.com>
2025-08-01 11:34:08 -0300

    feat: Add password service

---
+++ Helper/PasswordService.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Identity;
+using MinimalAPI.Domain.Entities;
+
+namespace MinimalAPI.Helper;
+
+public class PasswordService
+{
+    private readonly PasswordHasher<User> _passwordHasher = new();
+
+    public string HashPassword(User user, string password)
+    {
+        return _passwordHasher.HashPassword(user, password);
+    }
+
+    public bool VerifyPassword(User user, string password)
+    {
+        var result = _passwordHasher.VerifyHashedPassword(user, user.Password, password);
+        return result == PasswordVerificationResult.Success;
+    }
+}
