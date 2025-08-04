367e9d8b60018654e14ad1c75e7c273999a4f615
Joao Batista <joao.batista.jrs@gmail.com>
2025-08-04 14:54:14 -0300

    feat: Add generic repository

---
+++ Domain/Interfaces/Repository/IEntityRepository.cs
@@ -0,0 +1,12 @@
+﻿using MinimalAPI.Domain.Entities;
+using System.Linq.Expressions;
+
+namespace MinimalAPI.Domain.Interfaces.Repository
+{
+    public interface IEntityRepository<T> where T : class
+    {
+        Task<T> GetByIdAsync(int id);
+        Task<T> CreateAsync(T entity);
+        Task<PagedResult<T>> GetPagedAsync(Expression<Func<T, bool>>? filter, int pageNumber, int pageSize);
+    }
+}
---
+++ Infrasctructure/Repository/EntityRepository.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MinimalAPI.Domain.Entities;
+using MinimalAPI.Domain.Interfaces.Repository;
+using MinimalAPI.Infrasctructure.DataBase;
+using System.Linq.Expressions;
+
+namespace MinimalAPI.Infrasctructure.Repository
+{
+    public class EntityRepository<T> : IEntityRepository<T> where T : class
+    {
+        private readonly CarsContext _context;
+
+        public EntityRepository(CarsContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<PagedResult<T>> GetPagedAsync(Expression<Func<T, bool>>? filter, int pageNumber, int pageSize)
+        {
+            var query = _context.Set<T>().AsQueryable();
+
+            if (filter is not null)
+                query = query.Where(filter);
+
+            var totalCount = await query.CountAsync();
+
+            var items = await query
+                .Skip((pageNumber - 1) * pageSize)
+                .Take(pageSize)
+                .ToListAsync();
+
+            return new PagedResult<T>
+            {
+                Items = items,
+                PageNumber = pageNumber,
+                PageSize = pageSize,
+                TotalCount = totalCount
+            };
+        }
+
+        public async Task<T> GetByIdAsync(int id)
+        {
+            return await _context.Set<T>().FindAsync(id);
+        }
+
+        public async Task<T> CreateAsync(T entity)
+        {
+            _context.Set<T>().Add(entity);
+            await _context.SaveChangesAsync();
+            return entity;
+        }
+
+    }
+}
